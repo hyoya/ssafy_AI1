3fff9403942483dc965880ac55fbd3201183004c
hyoya <ghtybndss@gmail.com>
2019-08-20 10:17:44 +0900

    [S1P2241009-34] Req. 2-1-1 : pickle로 저장된 model.clf 파일 불러오기

--- sub1/app.py
+++ sub1/app.py
@@ -15,27 +15,30 @@ slack_events_adaptor = SlackEventAdapter(SLACK_SIGNING_SECRET, "/listening", app
 slack_web_client = WebClient(token=SLACK_TOKEN)
 
 # Req. 2-1-1. pickle로 저장된 model.clf 파일 불러오기
+with open("model.clf", "rb") as f : data = pickle.load(f)
 
-# Req. 2-1-2. 입력 받은 광고비 데이터에 따른 예상 판매량을 출력하는 lin_pred() 함수 구현
-def lin_pred(test_str):
-
-    return None
-
-# 챗봇이 멘션을 받았을 경우
-@slack_events_adaptor.on("app_mention")
-def app_mentioned(event_data):
-    channel = event_data["event"]["channel"]
-    text = event_data["event"]["text"]
-
-    keywords = lin_pred(text)
-    slack_web_client.chat_postMessage(
-        channel=channel,
-        text=keywords
-    )
-
-@app.route("/", methods=["GET"])
-def index():
-    return "<h1>Server is ready.</h1>"
-
-if __name__ == '__main__':
-    app.run()
+##print(f)
+##
+### Req. 2-1-2. 입력 받은 광고비 데이터에 따른 예상 판매량을 출력하는 lin_pred() 함수 구현
+##def lin_pred(test_str):
+##
+##    return None
+##
+### 챗봇이 멘션을 받았을 경우
+##@slack_events_adaptor.on("app_mention")
+##def app_mentioned(event_data):
+##    channel = event_data["event"]["channel"]
+##    text = event_data["event"]["text"]
+##
+##    keywords = lin_pred(text)
+##    slack_web_client.chat_postMessage(
+##        channel=channel,
+##        text=keywords
+##    )
+##
+##@app.route("/", methods=["GET"])
+##def index():
+##    return "<h1>Server is ready.</h1>"
+##
+##if __name__ == '__main__':
+##    app.run()
